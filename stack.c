8108a64f5751085657aa2f1f4276e62453ba13c2
edma2 <edma2@san.rr.com>
2011-01-19 10:27:00 -0800

    clean up

--- stack.c
+++ stack.c
@@ -6,47 +6,41 @@
  */
 
 Stack *stack_new(void) {
-        return NULL;
+    return NULL;
 }
 
 int stack_isempty(Stack **s) {
-        return (*s == NULL);
+    return (*s == NULL);
 }
 
 void *stack_top(Stack **s) {
-        if (stack_isempty(s))
-                return NULL;
-        return (*s)->item;
-}
-
-void stack_free(Stack **s) {
-        while (!stack_isempty(s))
-                stack_pop(s);
+    if (stack_isempty(s))
+        return NULL;
+    return (*s)->item;
 }
 
 /* Push new item onto stack */
 void stack_push(Stack **s, void *item) {
-        Stack *top;
+    Stack *top;
 
-        top = malloc(sizeof(Stack));
-        if (top == NULL)
-                return;
-        top->item = item;
-        top->next = *s;
-        *s = top;
+    top = malloc(sizeof(Stack));
+    if (top == NULL)
+        return;
+    top->item = item;
+    top->next = *s;
+    *s = top;
 }
 
 /* Pop stack and return item */
 void *stack_pop(Stack **s) {
-        Stack *next;
-        void *item;
-        
-        if (*s == NULL)
-                return NULL;
-        next = (*s)->next;
-        item = (*s)->item;
-        free(*s);
-        *s = next;
+    Stack *next;
+    void *item;
 
-        return item;
+    if (*s == NULL)
+        return NULL;
+    next = (*s)->next;
+    item = (*s)->item;
+    free(*s);
+    *s = next;
+    return item;
 }
--- stack.h
+++ stack.h
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 
 struct Stack {
-        struct Stack *next;
-        void *item;
+    struct Stack *next;
+    void *item;
 };
 typedef struct Stack Stack;
 
@@ -11,4 +11,3 @@ int stack_isempty(Stack **s);
 void *stack_top(Stack **s);
 void stack_push(Stack **s, void *item);
 void *stack_pop(Stack **s);
-void stack_free(Stack **s);
